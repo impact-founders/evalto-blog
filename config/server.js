63dc695cd4244de855f817e20f4b03b5baedbdba
Helene <helene.gloux@gmail.com>
2025-09-01 17:05:57 +0200

    setup local

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['yourKeyA', 'yourKeyB']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  proxy: env.bool('PROXY', false),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
   },
 });
